e85b779f2e993edb74e695c60d890f5f5b6fad62
xuzh <zihui_xu@126.com>
2017-05-01 16:11:04 +0800

    graduation

--- all-backstage-page/save-self-introduction.php
+++ all-backstage-page/save-self-introduction.php
@@ -20,13 +20,25 @@
         die('error'.mysqli_error);
     }
     mysqli_query($conn, "set names 'utf8'");
-    mysqli_select_db($conn, "graduation_data");  //打开数据库
-    $insert_action = "insert into personalinfo (name, email, github_id, blog_id, self_introduction) values ('$name', '$email', '$github_id', '$blog_id', '$introduction')";
-    $insert_result = mysqli_query($conn, $insert_action);
-    if($insert_result){
-        echo 0;  //信息插入成功
+    mysqli_select_db($conn, "graduation-data");  //打开数据库
+    $select_action = "select useremail from personalinfo where useremail = '$useremail'";
+    $select_result = mysqli_query($conn, $select_action);
+    if($select_result){
+        $update_action = "update personalinfo set name='$name',email='$email',github_id='$github_id',blog_id='$blog_id',self_introduction='$introduction'";
+        $update_result = mysqli_query($conn, $update_action);
+        if($update_result){
+            echo 2;  //信息更新成功
+        } else {
+            echo 3;  //信息更新失败
+        }
     } else {
-        echo 1;  //信息插入失败
+        $insert_action = "insert into personalinfo (name, email, github_id, blog_id, self_introduction, useremail) values ('$name', '$email', '$github_id', '$blog_id', '$introduction', '$useremail')";
+        $insert_result = mysqli_query($conn, $insert_action);
+        if($insert_result){
+            echo 0;  //信息插入成功
+        } else {
+            echo 1;  //信息插入失败
+        }
     }
 
 ?>
